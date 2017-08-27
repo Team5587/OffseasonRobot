622eb6e2d734f7a86b6faba524d252c3c5b570b9
Drew Fisher <drewsapple111@gmail.com>
2017-08-27 12:33:29 -0400

    Shifting fixes

--- src/org/usfirst/frc/team5587/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team5587/robot/subsystems/Drivetrain.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team5587.robot.subsystems;
 
-
 import org.usfirst.frc.team5587.robot.Robot;
 import com.ctre.CANTalon;
 
@@ -17,66 +16,74 @@ import org.usfirst.frc.team5587.robot.RobotMap;
 public class Drivetrain extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
-    private CANTalon leftFront, leftBack, rightFront, rightBack;
-    private Encoder leftEncoder, rightEncoder;
-    private Pneumatics p;
-    public RobotDrive drivetrain;
+	private CANTalon leftFront, leftBack, rightFront, rightBack;
+	private Encoder leftEncoder, rightEncoder;
+	private Pneumatics p;
+	public RobotDrive drivetrain;
 
-    // 1 / ( Correction constant * Stage 1 * Stage 2 * Wheel Diameter * pi ) 
-    private double kDistHighGear = 1 / (40/12D * 44/40D * 6 * Math.PI);
-    private double kDistLowGear = 1 / (40/12D * 60/24D * 6 * Math.PI);
+	// 1 / ( Correction constant * Stage 1 * Stage 2 * Wheel Diameter * pi )
+	private double kDistHighGear = 1 / (40 / 12D * 44 / 40D * 6 * Math.PI);
+	private double kDistLowGear = 1 / (40 / 12D * 60 / 24D * 6 * Math.PI);
 
-    public enum gear {
-    	High, Low, Disengaged
-    }
-    
-    gear leftGear, rightGear;
-    
-    public Drivetrain(){
-        leftFront = new CANTalon( RobotMap.LEFT_FRONT );
-        leftBack = new CANTalon( RobotMap.LEFT_BACK );
-        rightFront = new CANTalon( RobotMap.RIGHT_FRONT );
-        rightBack = new CANTalon( RobotMap.RIGHT_BACK );
+	public enum gear {
+		High, Low, Disengaged
+	}
 
-        leftEncoder = new Encoder( RobotMap.LEFT_ENCODER, RobotMap.LEFT_ENCODER+1 );
-        rightEncoder = new Encoder( RobotMap.RIGHT_ENCODER, RobotMap.RIGHT_ENCODER+1 );
-        
-        p = Robot.pneumatics;
-        
-        drivetrain = new RobotDrive(leftFront, leftBack, rightFront, rightBack);
-    }
+	gear leftGear, rightGear;
+
+	public Drivetrain() {
+		leftFront = new CANTalon(RobotMap.LEFT_FRONT);
+		leftBack = new CANTalon(RobotMap.LEFT_BACK);
+		rightFront = new CANTalon(RobotMap.RIGHT_FRONT);
+		rightBack = new CANTalon(RobotMap.RIGHT_BACK);
+
+		leftEncoder = new Encoder(RobotMap.LEFT_ENCODER, RobotMap.LEFT_ENCODER + 1);
+		rightEncoder = new Encoder(RobotMap.RIGHT_ENCODER, RobotMap.RIGHT_ENCODER + 1);
+
+		p = Robot.pneumatics;
+
+		drivetrain = new RobotDrive(leftFront, leftBack, rightFront, rightBack);
+	}
 
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
 	}
 
-    public void arcadeDrive( double power, double curve ){
-        drivetrain.arcadeDrive( power, curve );
-    }
+	public void arcadeDrive(double power, double curve) {
+		drivetrain.arcadeDrive(power, curve);
+	}
 
-    public void arcadeVelocityControl( double power, double curve){
-    	
-    }
-    
-    public void shiftInto(gear g){
-    	Value v = (g == gear.High) ? Value.kForward : Value.kReverse;
-    	p.setSolenoid( p.shifter, v );
-    }
-    
-    
-//    public double getDistance(){
-//    	leftEncoder.set
-//    }
+	public void arcadeVelocityControl(double power, double curve) {
+
+	}
+
+	public void shiftInto(gear g) {
+		Value v;
+	       	switch(g){
+		case gear.High:
+			v = Value.kForward;
+			break;
+		case gear.Low:
+			v = Value.kReverse;
+			break;
+		case gear.Disengaged;
+			v = Value.kOff;
+			break;
+		}
+		p.setSolenoid(p.shifter, v);
+	}
+
+	// public double getDistance(){
+	// leftEncoder.set
+	// }
 
 	public gear getGear() {
-		if(p.shifter.get() == Value.kForward){
+		if (p.shifter.get() == Value.kForward) {
 			return gear.High;
-		}
-		else if(p.shifter.get() == Value.kReverse){
+		} else if (p.shifter.get() == Value.kReverse) {
 			return gear.Low;
-		}
-		else{
+		} else {
 			return gear.Disengaged;
 		}
 	}
--- src/org/usfirst/frc/team5587/robot/subsystems/Pneumatics.java
+++ src/org/usfirst/frc/team5587/robot/subsystems/Pneumatics.java
@@ -16,41 +16,43 @@ import org.usfirst.frc.team5587.robot.RobotMap;
 public class Pneumatics extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
-    private Compressor comp;
-    public DoubleSolenoid shifter;
+	private Compressor comp;
+	public DoubleSolenoid shifter;
 
-    public Pneumatics(){
-        comp = new Compressor(0);
-        shifter = new DoubleSolenoid(RobotMap.LEFT_SHIFTER_A, RobotMap.LEFT_SHIFTER_B);
-    }
+	public Pneumatics() {
+		comp = new Compressor(0);
+		shifter = new DoubleSolenoid(RobotMap.SHIFTER_A, RobotMap.SHIFTER_B);
+	}
 
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
-        setDefaultCommand( new PneumaticsOverhead() );
+		setDefaultCommand(new PneumaticsOverhead());
 	}
 
-    public void enableClosedLoop(boolean enabled){
-        comp.setClosedLoopControl( enabled );
-    }
+	public void enableClosedLoop(boolean enabled) {
+		comp.setClosedLoopControl(enabled);
+	}
 
-    public void setSolenoid(Solenoid s, boolean value){
-        s.set( value );
-    }
-    public void setSolenoid(DoubleSolenoid s, Value value){
-        s.set( value );
-    }
-    public void setSolenoid(DoubleSolenoid s, int value){
-        switch(value){
-            case -1: 
-                s.set(Value.kReverse);
-                break;
-            case 0: 
-                s.set(Value.kOff);
-                break;
-            case 1: 
-                s.set(Value.kForward);
-                break;
-        }
-    }
+	public void setSolenoid(Solenoid s, boolean value) {
+		s.set(value);
+	}
+
+	public void setSolenoid(DoubleSolenoid s, Value value) {
+		s.set(value);
+	}
+
+	public void setSolenoid(DoubleSolenoid s, int value) {
+		switch (value) {
+		case -1:
+			s.set(Value.kReverse);
+			break;
+		case 0:
+			s.set(Value.kOff);
+			break;
+		case 1:
+			s.set(Value.kForward);
+			break;
+		}
+	}
 }
