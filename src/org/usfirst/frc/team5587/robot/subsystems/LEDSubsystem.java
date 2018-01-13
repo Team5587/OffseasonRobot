dcacbff97b4033bc30b6cb58a4e3a2786ebb3cf0
Drew Fisher <drewsapple111@gmail.com>
2018-01-13 17:10:45 -0500

    Add RGB LED subsystem

---
+++ src/org/usfirst/frc/team5587/robot/subsystems/LEDSubsystem.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team5587.robot.subsystems;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class LEDSubsystem extends Subsystem {
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	private I2C arduino;
+	public LEDSubsystem(){
+		arduino = new I2C(Port.kMXP, 8);
+	}
+
+	public void sendRGB(int r, int g, int b){
+		byte[] color = new byte[3];
+		//These bytes are SIGNED (-127,127) they are converted to unsigned later by the arduino
+		color[1] = (byte)r;
+		color[2] = (byte)g;
+		color[3] = (byte)b;
+		byte[] received = new byte[1];
+		arduino.transaction(color, 3, received, 0);
+	}
+
+	public void initDefaultCommand() {
+	}
+}
