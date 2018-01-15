b7f7ff5bbd5344b13834f1df8218b8c7ed1cbdb4
Gcambridge <gavrielcambridge@gmail.com>
2018-01-15 11:09:56 -0500

    Underlighting LED Subystem

--- src/org/usfirst/frc/team5587/robot/subsystems/LEDSubsystem.java
+++ src/org/usfirst/frc/team5587/robot/subsystems/LEDSubsystem.java
@@ -11,17 +11,19 @@ public class LEDSubsystem extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 	private I2C arduino;
+
 	public LEDSubsystem(){
 		arduino = new I2C(Port.kMXP, 8);
 	}
 
 	public void sendRGB(int r, int g, int b){
 		byte[] color = new byte[3];
+
 		//These bytes are SIGNED (-127,127) they are converted to unsigned later by the arduino
 		color[1] = (byte)r;
 		color[2] = (byte)g;
 		color[3] = (byte)b;
-		byte[] received = new byte[1];
+		byte[] received = {(byte)0}; //declared and initailized but never used, as null can not be passed as a parameter. This value is never used.
 		arduino.transaction(color, 3, received, 0);
 	}
 
